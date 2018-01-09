e6bf299fbabbd294b518d34adfe9fdbfe2bc8b49
backend <backendidsiapps@gmail.com>
2018-01-09 17:24:07 +0700

    update

--- src/AdminPanelServiceProvider.php
+++ src/AdminPanelServiceProvider.php
@@ -23,6 +23,7 @@ class AdminPanelServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
         $this->loadViewsFrom(__DIR__ . '/views/', 'admin');
 
+
 //        $this->publishes([
 //            __DIR__ . '/assets/admin.css'      => public_path('css/admin.css'),
 //            __DIR__ . '/assets/admin.js'       => public_path('js/admin/admin.js'),
