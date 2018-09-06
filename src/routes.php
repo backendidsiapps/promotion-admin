8bd4b23e3a606885a676c25107d385a778189d4b
backendidsiapps <35249067+backendidsiapps@users.noreply.github.com>
2018-09-06 12:23:28 +0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route; +
 
 Route::group(
     ['namespace' => 'AdminPanel\Controllers', 'prefix' => 'admin', 'middleware' => ['web']], function () {
